7f149de3b3c26f5eca0ac805f999264c443720d7
dongnguyen <dongnh@vakaxa.com>
2018-09-11 15:52:47 +0700

    ETH RPC Test

---
+++ Vakapay.UnitTest/Vakapay.VakacoinBusiness.Test/ETHRPCTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Vakapay.EthereumBusiness;
+
+namespace Vakapay.VakacoinBusiness.Test
+{
+
+	[TestFixture]
+	class ETHRPCTest
+	{
+
+		EthereumRpc _etheRpc;
+		[SetUp]
+		public void Setup()
+		{
+			_etheRpc = new EthereumRpc("HELOO");
+		}
+
+		[TestCase(0)]
+		public void GetBlock(int id)
+		{
+			var output = _etheRpc.FindBlockByNumber(id);
+			//Trace.Write(JsonHelper.SerializeObject(output));
+			//	Console.WriteLine(JsonHelper.SerializeObject(output));
+			Assert.IsNotNull(output);
+		}
+
+	}
+}
