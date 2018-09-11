5bc827a940cf9885c714886cbc9bae78d5d43ceb
Tieu Thanh Liem <tieuthanhliem@gmail.com>
2018-09-11 10:58:06 +0700

    add unit test for VakacoinRpc

--- Vakapay.VakacoinBusiness/VakacoinRpc.cs
+++ Vakapay.VakacoinBusiness/VakacoinRpc.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Newtonsoft.Json.Linq;
 using Vakapay.Models.Domains;
 using Vakapay.Commons.Helpers;
@@ -55,8 +57,74 @@ namespace Vakapay.VakacoinBusiness
 
         public VakaKeyPair CreateKey()
         {
-             byte[] keybytes = Secp256K1Manager.GenerateRandomKey();
+            byte[] keybytes = Secp256K1Manager.GenerateRandomKey();
+            string prv = KeyToString(keybytes, "sha256x2");
             return new VakaKeyPair();
         }
+        
+        public static string KeyToString(byte[] key, string keyType, string prefix = null)
+        {
+            byte[] digest = null;
+
+            if (keyType == "sha256x2")
+            {
+                digest = Sha256Manager.GetHash(Sha256Manager.GetHash(Combine(new List<byte[]>() {
+                    new byte[] { 128 },
+                    key
+                })));
+            }
+            else if (!string.IsNullOrWhiteSpace(keyType))
+            {
+                digest = Ripemd160Manager.GetHash(Combine(new List<byte[]>() {
+                    key,
+                    Encoding.UTF8.GetBytes(keyType)
+                }));
+            }
+            else
+            {
+                digest = Ripemd160Manager.GetHash(key);
+            }
+
+            return prefix + Base58.Encode(Combine(new List<byte[]>() {
+                key,
+                digest.Take(4).ToArray()
+            }));
+        }
+        
+        public static byte[] Combine(IEnumerable<byte[]> arrays)
+        {
+            byte[] ret = new byte[arrays.Sum(x => x != null ? x.Length : 0)];
+            int offset = 0;
+            foreach (byte[] data in arrays)
+            {
+                if (data == null) continue;
+
+                Buffer.BlockCopy(data, 0, ret, offset, data.Length);
+                offset += data.Length;
+            }
+            return ret;
+        }
+
+        public void MainTest()
+        {
+            
+        }
+     
+//        private static object ReadPrivateKey(byte[] data, ref Int32 readIndex)
+//        {
+//            var type = (byte)ReadByte(data, ref readIndex);
+//            var keyBytes = data.Skip(readIndex).Take(CryptoHelper.PRIV_KEY_DATA_SIZE).ToArray();
+//
+//            readIndex += CryptoHelper.PRIV_KEY_DATA_SIZE;
+//
+//            if (type == (int)KeyType.r1)
+//            {
+//                return CryptoHelper.PrivKeyBytesToString(keyBytes, "R1", "PVT_R1_");
+//            }
+//            else
+//            {
+//                throw new Exception("private key type not supported.");
+//            }
+//        }
     }
 }
