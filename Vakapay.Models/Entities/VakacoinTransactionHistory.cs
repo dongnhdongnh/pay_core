a4154928f77cdc242b88cb7417e74657c64b8c4d
minhbv <minhbv@vakaxa.com>
2018-09-12 17:09:42 +0700

    add VakacoinTransactionHistory

---
+++ Vakapay.Models/Entities/VakacoinTransactionHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vakapay.Models.Entities
+{
+    [Table("VakacoinTransactionHistory")]
+    public class VakacoinTransactionHistory
+    {
+        public string Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public Decimal Amount { get; set; }
+        public DateTime TransactionTime { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public int status { get; set; }
+    }
+}
