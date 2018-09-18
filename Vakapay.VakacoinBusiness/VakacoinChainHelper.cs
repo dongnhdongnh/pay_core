bae7c93ba409cbc3840e7a16941d28532b783a5d
minhbv <minhbv@vakaxa.com>
2018-09-18 15:32:39 +0700

    change vakacoinChainHelper

--- Vakapay.VakacoinBusiness/VakacoinChainHelper.cs
+++ Vakapay.VakacoinBusiness/VakacoinChainHelper.cs
@@ -62,62 +62,68 @@ namespace Vakapay.ScanVakaCoin
                 {
                     continue;
                 }
-//                var jsonTrx = JObject.Parse(transaction.ToString())["transaction"];
-//
-//                //check name of action in transaction
-//                var actionName = jsonTrx["actions"][0]["name"];
-//                if (actionName == null)
-//                    continue;
-//                if (actionName.ToString() != "transfer")
-//                    continue;
-//
-//                //check data is valid json
-//                if (!JsonHelper.IsValidJson(jsonTrx["actions"][0]["data"].ToString()))
-//                    continue;
-//
-//                //check quantity and to_address in transaction
-//                var quantity = jsonTrx["actions"][0]["data"]["quantity"];
-//                var _to = jsonTrx["actions"][0]["data"]["to"];
-//
-//                //if quantity == null, process next transaction in arraylist
-//                if (quantity == null || _to == null)
+//                // process each transaction in arraylist of transactions
+//            foreach (var transaction in transactions)
+//            {
+//                try
 //                {
-//                    continue;
-//                }
-//                
-//                var trxId = JObject.Parse(transaction.ToString())["id"].ToString();
+//                    var jsonTrx = JObject.Parse(transaction.ToString())["transaction"];
 //
-//                string _quantity = quantity.ToString();
-//                if (!String.IsNullOrEmpty(_quantity))
-//                {
-//                    //check symbol in quantity
-//                    var symbol = _quantity.Split(" ")[1];
-//                    if (symbol == "EOS" || symbol == "VAKA")
+//                    //check name of action in transaction
+//                    var actionName = jsonTrx["actions"][0]["name"];
+//                    if (actionName == null)
+//                        continue;
+//                    if (actionName.ToString() != "transfer")
+//                        continue;
+//
+//                    //check data is valid json
+//                    if (!JsonHelper.IsValidJson(jsonTrx["actions"][0]["data"].ToString()))
+//                        continue;
+//
+//                    //check quantity and to_address in transaction
+//                    var quantity = jsonTrx["actions"][0]["data"]["quantity"];
+//                    var _to = jsonTrx["actions"][0]["data"]["to"];
+//
+//                    //if quantity == null, process next transaction in arraylist
+//                    if (quantity == null || _to == null)
 //                    {
-//                        string to = _to.ToString();
+//                        continue;
+//                    }
 //
-//                        // if receiver doesn't exist in wallet table, process next transaction
-//                        if (!_walletBusiness.CheckExistedAddress(to))
+//                    var trxId = JObject.Parse(transaction.ToString())["id"].ToString();
+//
+//                    string _quantity = quantity.ToString();
+//                    if (!String.IsNullOrEmpty(_quantity))
+//                    {
+//                        //check symbol in quantity
+//                        var symbol = _quantity.Split(" ")[1];
+//                        if (symbol == "EOS" || symbol == "VAKA")
 //                        {
-//                            logger.Info(to + " is not exist in Wallet!!!");
-//                            continue;
+//                            string to = _to.ToString();
+//                            string from = jsonTrx["actions"][0]["data"]["from"].ToString();
+//                            decimal amount = decimal.Parse(_quantity.Split(" ")[0]);
+//                            string transactionTime = jsonTrx["expiration"].ToString();
+//                            string status = "success";
+//
+//                            // if receiver doesn't exist in wallet table, process next transaction
+//                            if (!_walletBusiness.CheckExistedAndUpdateByAddress(to, amount, symbol))
+//                                continue;
+//
+//                            // save to VakacoinTransactionHistory
+//                            ReturnObject result =
+//                                _vakacoinBusiness.CreateTransactionHistory(trxId, from, to, blockNumber, amount,
+//                                    transactionTime, status);
+//
+//                            logger.Info(to + " was received " + amount + " " + symbol);
 //                        }
-//
-//                        string from = jsonTrx["actions"][0]["data"]["from"].ToString();
-//                        decimal amount = decimal.Parse(_quantity.Split(" ")[0]);
-//                        string transactionTime = jsonTrx["expiration"].ToString();
-//                        string status = "success";
-//
-//                        // save to VakacoinTransactionHistory
-//                        ReturnObject result =
-//                            _vakacoinBusiness.CreateTransactionHistory(trxId, from, to, Int32.Parse(block.BlockNum.ToString()), amount, transactionTime, status);
-//
-//                        //update Balance in Wallet
-//                        _walletBusiness.UpdateBalance(to, amount, symbol);
-//
-//                        logger.Info(to + " was received " + amount + " " + symbol);
 //                    }
 //                }
+//                catch (Exception e)
+//                {
+//                    logger.Error(e);
+//                    throw;
+//                }
+//            }
             }
         }
     }
