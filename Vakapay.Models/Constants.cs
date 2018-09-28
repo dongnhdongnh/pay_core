8657a6400701ceffb3f5d7c7bf1b7203678838f4
tienchelsea92 <tienpt@vakaxa.com>
2018-09-28 10:04:31 +0700

    add class constants

---
+++ Vakapay.Models/Constants.cs
@@ -0,0 +1,10 @@
+namespace Vakapay.Models
+{
+    public class Constants
+    {
+        public const string TYPE_COIN_BITCOIN = "Bitcoin";
+
+        public const int TYPE_EMAIL_SEND = 1;
+        public const int TYPE_EMAIL_RECEIVER = 2;
+    }
+}
