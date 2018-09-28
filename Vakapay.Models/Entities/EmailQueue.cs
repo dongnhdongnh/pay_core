18e4e2b1419ebf3cb8e15d8c29252348e781b7bf
minhbv <minhbv@vakaxa.com>
2018-09-28 10:17:02 +0700

    update email queue object

--- Vakapay.Models/Entities/EmailQueue.cs
+++ Vakapay.Models/Entities/EmailQueue.cs
@@ -8,9 +8,23 @@ namespace Vakapay.Models.Entities
     {
         public string Id { get; set; }
         public string ToEmail { get; set; }
-        public string Content { get; set; }
         public string Subject { get; set; }
-        public string Template { get; set; }
+        public string Template { get; set; } //value = "new device", "sent", "verify"
+        
+        //new device template
+        public string DeviceLocation { get; set; }
+        public string DeviceIP { get; set; }
+        public string DeviceBrowser { get; set; }
+        public string DeviceAuthorizeUrl { get; set; }
+        
+        // Sent/Received template
+        public decimal Amount { get; set; }
+        public string SentOrReceived { get; set; } //value = "sent" or "received"
+        public string NetworkName { get; set; }
+        
+        //Verify email template
+        public string VerifyUrl { get; set; }
+        
         public string Status { get; set; }
         public long CreatedAt { get; set; }
         public long UpdatedAt { get; set; }
