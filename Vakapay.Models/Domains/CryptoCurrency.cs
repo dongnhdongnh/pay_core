326a76e514e27b40428db3b81a1275215f224795
Tieu Thanh Liem <tieuthanhliem@gmail.com>
2018-10-17 14:24:03 +0700

    update AccountsController: GetBalance:fix wrong currency symbole caused by Nam Dynamo, GetTransactions: Check limit < list count before cutting the list

--- Vakapay.Models/Domains/CryptoCurrency.cs
+++ Vakapay.Models/Domains/CryptoCurrency.cs
@@ -8,15 +8,22 @@ namespace Vakapay.Models.Domains
         public const string ETH = "Ethereum";
         public const string VAKA = "Vakacoin";
         public const string BTC = "Bitcoin";
+
+        private static Dictionary<string, string> CurrencySymbol { get; } = new Dictionary<string, string>()
+        {
+            {ETH, "ETH"},
+            {BTC, "BTC"},
+            {VAKA, "VAKA"}
+        };
         
         public static string GetAmount(string currency, decimal amount)
         {
             if (currency == VAKA)
             {
-                return amount.ToString("N4") + " " + nameof(currency);
+                return amount.ToString("N4") + " " + CurrencySymbol[currency];
             }
 
-            return amount + " " + nameof(currency);
+            return amount + " " + CurrencySymbol[currency];
         }
     }
 }
--- Vakapay.Models/Domains/ReturnObject.cs
+++ Vakapay.Models/Domains/ReturnObject.cs
@@ -4,8 +4,13 @@ namespace Vakapay.Models.Domains
 {
     public class ReturnObject
     {
+        [JsonProperty(Order = 1)]
         public string Status { get; set; }
+
+        [JsonProperty(Order = 2)]
         public string Message { get; set; }
+
+        [JsonProperty(Order = 3)]
         public string Data { get; set; }
 
         public string ToJson() =>
@@ -13,5 +18,11 @@ namespace Vakapay.Models.Domains
 
         public static ReturnObject FromJson(string json) =>
             JsonHelper.DeserializeObject<ReturnObject>(json, JsonHelper.ConvertSettings);
+    }
+
+    public class ReturnDataObject : ReturnObject
+    {
+        [JsonProperty(Order = 4)]
+        public new object Data { get; set; }
     }
 }
