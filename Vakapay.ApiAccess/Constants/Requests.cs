e0a1aac6c5582e0f632acfa933b03774955f5186
tienchelsea92 <tienpt@vakaxa.com>
2018-10-23 16:35:29 +0700

    update add project apiAccess

---
+++ Vakapay.ApiAccess/Constants/Requests.cs
@@ -0,0 +1,8 @@
+namespace Vakapay.ApiAccess.Constants
+{
+    public class Requests
+    {
+        public static string HeaderApiSecret = "API_SECRET";
+        public static string HeaderApiKey = "API_KEY";
+    }
+}
