8f96ea7a04c104327503ec31fb13c3c4c98b6205
tienchelsea92 <tienpt@vakaxa.com>
2018-10-25 09:00:23 +0700

    add api "get list address by user"

--- Vakapay.ApiAccess/Controllers/AddressController.cs
+++ Vakapay.ApiAccess/Controllers/AddressController.cs
@@ -1,17 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Vakapay.ApiAccess.ActionFilter;
 using Vakapay.ApiAccess.Constants;
+using Vakapay.ApiServer.Helpers;
 using Vakapay.Commons.Constants;
 using Vakapay.Commons.Helpers;
 using Vakapay.Models.Domains;
+using Vakapay.Models.Entities;
 using Vakapay.Models.Repositories;
 using Vakapay.Repositories.Mysql;
 
 namespace Vakapay.ApiAccess.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("v1/[controller]")]
+    [BaseActionFilter]
     [ApiController]
     public class AddressController : ControllerBase
     {
@@ -34,19 +39,18 @@ namespace Vakapay.ApiAccess.Controllers
         /// <param name="idAddress"></param>
         /// <param name="currency"></param>
         /// <returns></returns>
-        [BaseActionFilter]
-        [HttpGet("getAddress/{idAddress}/{currency}")]
+        [HttpGet("{idAddress}/{currency}")]
         public ActionResult<string> GetAddressInfoById(string idAddress, string currency)
         {
             try
             {
-                if (string.IsNullOrEmpty(idAddress) || string.IsNullOrEmpty(currency))
+                if (string.IsNullOrEmpty(idAddress) || string.IsNullOrEmpty(currency) || !ApiAccessHelper.ValidateId(idAddress))
                 {
                     return CreateDataError(MessageError.ParamInvalid);
                 }
 
                 BlockchainAddress blockChainAddress;
-                switch (currency)
+                switch (currency.ToUpper().Trim())
                 {
                     case CryptoCurrency.BTC:
                         var bitCoinAddressRepository =
@@ -67,9 +71,16 @@ namespace Vakapay.ApiAccess.Controllers
                         return CreateDataError(MessageError.ParamInvalid);
                 }
 
-                return blockChainAddress != null
-                    ? CreateDataSuccess(JsonConvert.SerializeObject(blockChainAddress))
-                    : CreateDataError(MessageError.DataNotFound);
+                if (blockChainAddress?.WalletId == null) return CreateDataError(MessageError.DataNotFound);
+                var userModel = (User) RouteData.Values["UserModel"];
+                var walletBusiness = new WalletBusiness.WalletBusiness(VakapayRepositoryFactory);
+                var walletModel = walletBusiness.GetWalletByID(blockChainAddress.WalletId);
+                if (walletModel != null && string.Equals(walletModel.UserId, userModel.Id))
+                {
+                    return CreateDataSuccess(JsonConvert.SerializeObject(blockChainAddress));
+                }
+
+                return CreateDataError(MessageError.DataNotFound);
             }
             catch (Exception e)
             {
@@ -77,6 +88,68 @@ namespace Vakapay.ApiAccess.Controllers
                 return CreateDataError(MessageError.DataNotFound);
             }
         }
+
+        [HttpGet("list/{currency}")]
+        public ActionResult<string> GetListAddress(string currency)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(currency))
+                {
+                    return CreateDataError(MessageError.ParamInvalid);
+                }
+
+                var userModel = (User) RouteData.Values["UserModel"];
+                List<BlockchainAddress> listBlockChainAddress = null;
+                switch (currency.ToUpper().Trim())
+                {
+                    case CryptoCurrency.BTC:
+                        var bitCoinAddressRepository =
+                            new BitcoinAddressRepository(VakapayRepositoryFactory.GetOldConnection());
+                        var listBitCoinAddress =
+                            bitCoinAddressRepository.FindByUserIdAndCurrency(userModel.Id, currency);
+                        if (listBitCoinAddress != null)
+                        {
+                            listBlockChainAddress = listBitCoinAddress.Cast<BlockchainAddress>().ToList();
+                        }
+
+                        break;
+                    case CryptoCurrency.ETH:
+                        var ethereumAddressRepository =
+                            new EthereumAddressRepository(VakapayRepositoryFactory.GetOldConnection());
+                        var listEthereumAddress =
+                            ethereumAddressRepository.FindByUserIdAndCurrency(userModel.Id, currency);
+                        if (listEthereumAddress != null)
+                        {
+                            listBlockChainAddress = listEthereumAddress.Cast<BlockchainAddress>().ToList();
+                        }
+
+                        break;
+                    case CryptoCurrency.VAKA:
+                        var vaKaCoinAccountRepository =
+                            new VakacoinAccountRepository(VakapayRepositoryFactory.GetOldConnection());
+                        var listVaKaCoinAddress =
+                            vaKaCoinAccountRepository.FindByUserIdAndCurrency(userModel.Id, currency);
+                        if (listVaKaCoinAddress != null)
+                        {
+                            listBlockChainAddress = listVaKaCoinAddress.Cast<BlockchainAddress>().ToList();
+                        }
+
+                        break;
+                    default:
+                        return CreateDataError(MessageError.ParamInvalid);
+                }
+
+                return listBlockChainAddress != null
+                    ? CreateDataSuccess(JsonConvert.SerializeObject(listBlockChainAddress))
+                    : CreateDataError(MessageError.DataNotFound);
+            }
+            catch (Exception)
+            {
+                return CreateDataError(MessageError.DataNotFound);
+            }
+        }
+
 
         /// <summary>
         /// CreateDataError
--- Vakapay.Models/Repositories/Base/IAddressRepository.cs
+++ Vakapay.Models/Repositories/Base/IAddressRepository.cs
@@ -7,7 +7,8 @@ namespace Vakapay.Models.Repositories.Base
     public interface IAddressRepository<TBlockchainAddress>
     {
         TBlockchainAddress FindByAddress(string address);
-        List<TBlockchainAddress> FindByWalletId(string walletId);
+        IEnumerable<TBlockchainAddress> FindByWalletId(string walletId);
+        List<TBlockchainAddress> FindByUserIdAndCurrency(string user, string currency);
         Task<ReturnObject> InsertAddress(string address, string walletId, string other);
     }
 }
