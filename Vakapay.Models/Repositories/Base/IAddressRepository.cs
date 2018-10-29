9d2a6ac19eba542a691f61abf6a1fdc6f56f53c7
Ngô Ngọc Huấn <huannn@vakaxa.com>
2018-10-29 10:15:59 +0700

    esit get add by wallet to List

--- Vakapay.Models/Repositories/Base/IAddressRepository.cs
+++ Vakapay.Models/Repositories/Base/IAddressRepository.cs
@@ -7,7 +7,7 @@ namespace Vakapay.Models.Repositories.Base
     public interface IAddressRepository<TBlockchainAddress>
     {
         TBlockchainAddress FindByAddress(string address);
-        IEnumerable<TBlockchainAddress> FindByWalletId(string walletId);
+        List<TBlockchainAddress> FindByWalletId(string walletId);
         List<TBlockchainAddress> FindByUserIdAndCurrency(string user, string currency);
         Task<ReturnObject> InsertAddress(string address, string walletId, string other);
     }
